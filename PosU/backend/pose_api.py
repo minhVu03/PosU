df0ca3c041766c5a313255eed61445f969b12bcf
minhVu03 <minhvuams@gmail.com>
2025-03-08 22:01:51 -0800

    no more df parsing errors, but errors in front end jsx

--- PosU/backend/pose_api.py
+++ PosU/backend/pose_api.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
+import numpy as np
 import pandas as pd
 import json
 
 router = APIRouter(tags=["pose"])
 
 @router.post("/check_pose0")
-async def process_pose(request: Request):
+async def check_pose0(request: Request):
     """
     Receives pose landmarks and processes them.
     Expected request format:
@@ -26,8 +27,8 @@ async def process_pose(request: Request):
 
     success = 0
     #extract x and y from df
-    rightElbowPoint = np.array((df[13][0], df[13][1]))
-    rightWrist = np.array((df[15][0], df[15][1]))
+    rightElbowPoint = np.array((df.iloc[13]["landmarks"]["x"], df.iloc[13]["landmarks"]["y"]))
+    rightWrist = np.array((df.iloc[15]["landmarks"]["x"], df.iloc[15]["landmarks"]["y"]))
 
     point1 = np.array((0.8, 0.2)) #remember this for front end
     point2 = np.array((0.8, 0.7)) #remember this for front end
@@ -36,6 +37,7 @@ async def process_pose(request: Request):
     dist2 = np.linalg.norm(rightElbowPoint - point2)
     if(dist1 < 0.05 and dist2 < 0.05):
         success = 1
+        print("POSE 1 COMPLETED")
     
     # Example: Compute average visibility
     # avg_visibility = df["visibility"].mean()
@@ -50,7 +52,7 @@ async def process_pose(request: Request):
 
 
 @router.post("/check_pose1")
-async def process_pose(request: Request):
+async def check_pose1(request: Request):
     """
     Receives pose landmarks and processes them.
     Expected request format:
@@ -69,13 +71,13 @@ async def process_pose(request: Request):
 
     success = 0
     #extract x and y from df
-    rightElbowPoint = np.array((df[13][0], df[13][1]))
-    rightWrist = np.array((df[15][0], df[15][1]))
+    rightElbowPoint = np.array((df.iloc[13]["landmarks"]["x"], df.iloc[13]["landmarks"]["y"]))
+    rightWrist = np.array((df.iloc[15]["landmarks"]["x"], df.iloc[15]["landmarks"]["y"]))
 
     point1 = np.array((0.8, 0.2)) #remember this for front end
     point2 = np.array((0.5, 0.6)) #remember this for front end
 
-    dist1 = np.linalg.norm(rightElbowPoint - point1)
+    dist1 = np.linalg.norm(rightElbowPoint - point1) #WRIST NOT ELBOW
     dist2 = np.linalg.norm(rightElbowPoint - point2)
     if(dist1 < 0.05 and dist2 < 0.05):
         success = 1
@@ -92,7 +94,7 @@ async def process_pose(request: Request):
     return JSONResponse(content=jsonable_encoder(response_data))
 
 @router.post("/check_pose2")
-async def process_pose(request: Request):
+async def check_pose2(request: Request):
     """
     Receives pose landmarks and processes them.
     Expected request format:
@@ -107,12 +109,12 @@ async def process_pose(request: Request):
     # df = pd.DataFrame(data["landmarks"])
     df = pd.DataFrame(data)
 
-    print(df)
+    print(df.columns)
 
     success = 0
     #extract x and y from df
-    rightElbowPoint = np.array((df[13][0], df[13][1]))
-    rightWrist = np.array((df[15][0], df[15][1]))
+    rightElbowPoint = np.array((df.iloc[13]["landmarks"]["x"], df.iloc[13]["landmarks"]["y"]))
+    rightWrist = np.array((df.iloc[15]["landmarks"]["x"], df.iloc[15]["landmarks"]["y"]))
 
     point1 = np.array((0.8, 0.2)) #remember this for front end
     point2 = np.array((0.3, 0.2)) #remember this for front end
